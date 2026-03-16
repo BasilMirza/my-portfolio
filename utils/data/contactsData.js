87e39f5bde76b88e1e02784835d9c563f116044a
BasilMirza <basilmirza97@gmail.com>
2026-03-16 13:19:28 -0500

    small changes

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
-    facebook: 'https://www.facebook.com/abusaid.riyaz/',
-    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-    twitter: 'https://twitter.com/said7388',
-    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
-    devUsername: "said7388"
+    email: 'basilmirza14@gmail.com',
+    phone: '+1 (331) 627-0196',
+    address: 'Chicago, IL, USA',
+    github: 'https://github.com/BasilMirza',
+    facebook: 'https://www.facebook.com/basil.mirza.5',
+    linkedIn: 'https://www.linkedin.com/in/basilmirza',
+    twitter: 'https://x.com/BasilMirzaa',
+    stackOverflow: 'https://stackoverflow.com/users/16242515/basil-mirza',
+    devUsername: ""
 }
